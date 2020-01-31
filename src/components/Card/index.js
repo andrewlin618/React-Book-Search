be4cb45626f4e1eca4eb7aada6816048f3084ddd
Andrew Lin <andrewlin618@gmail.com>
2020-01-31 13:15:14 -0600

    aligned the buttons

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -1,6 +1,5 @@
 import React from "react";
 import {Button1,Button2} from "../Button";
-// import {Link} from "react-router-dom";
 import './style.css';
 // import { Link } from "react-router-dom";
 
@@ -10,7 +9,8 @@ class Card extends React.Component{
         authors: this.props.authors,
         description: this.props.description,
         image: this.props.image,
-        link: this.props.link
+        link: this.props.link,
+        target:"_blank"
     }
 
     handleSave = () =>{
@@ -23,16 +23,19 @@ class Card extends React.Component{
             <div className='card-body'>
                 <div className='row'>
                     <div className='col-md-4'>
-                        <a className='title-text' href={this.state.link}>{this.state.title}</a>
+                        <a className='title-text' href={this.state.link} target={this.state.target}>{this.state.title}</a>
                         <p>Written by: <span>{this.state.authors.toString()}</span></p>
                         <img src={this.state.image} alt={this.state.title}/>
                         <br /><br />
+
                     </div>
                     <div className='col-md-8'>
-                        <Button1 className='btn btn-primary mr-1' link={this.state.link}>view</Button1>
-                        <Button2 className='btn btn-success' onClick={this.handleSave}>save</Button2>
-                        <br /><br />
                         <p>{this.state.description}</p>
+                        <div style={{textAlign: 'right'}}>
+                            <Button1 className='btn btn-primary mr-1' link={this.state.link} target={this.state.target}>view</Button1>
+                            <Button2 className='btn btn-success' onClick={this.handleSave}>save</Button2>
+                        </div>
+                        <br /><br />
                     </div>
                 </div>
             </div>
